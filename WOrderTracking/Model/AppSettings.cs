d28208435d7d07c64f882bb4140be1e77a26256d
Allan Douglas R. de Oliveira <allandouglas@gmail.com>
2013-07-24 20:52:44 -0300

    - Settings are now persisted

---
+++ WOrderTracking/Model/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOrderTracking.Model
+{
+    public class AppSettings
+    {
+        public bool EnableNotifications { get; set; }
+        public int NotificationsInterval { get; set; }
+    }
+}
--- WOrderTracking/Settings.xaml.cs
+++ WOrderTracking/Settings.xaml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.ApplicationSettings;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -12,14 +13,37 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WOrderTracking.Model;
 
 namespace WOrderTracking
 {
     public sealed partial class Settings : UserControl
     {
+        private static Windows.Storage.ApplicationDataContainer roamingSettings =
+                Windows.Storage.ApplicationData.Current.RoamingSettings;
         public Settings()
         {
             this.InitializeComponent();
+            var settings = LoadSettings();
+            NotificationsSwitch.DataContext = settings;
+            NotificationsIntervalSlider.DataContext = settings;
+        }
+
+        private AppSettings LoadSettings()
+        {
+            var settingsValue = roamingSettings.Values["settings"] as ApplicationDataCompositeValue;
+            if (settingsValue != null)
+                return new AppSettings() { EnableNotifications = (bool)settingsValue["enableNotifications"], NotificationsInterval = (int)settingsValue["notificationsInterval"] };
+            else
+                return new AppSettings();
+        }
+
+        private void SaveSettings(AppSettings settings)
+        {
+            var settingsValue = new ApplicationDataCompositeValue();
+            settingsValue["enableNotifications"] = settings.EnableNotifications;
+            settingsValue["notificationsInterval"] = settings.NotificationsInterval;
+            roamingSettings.Values["settings"] = settingsValue;
         }
 
         private void MySettingsBackClicked(object sender, RoutedEventArgs e)
@@ -30,5 +54,19 @@ namespace WOrderTracking
             }
             SettingsPane.Show();
         }
+
+        private void ToggleSwitch_Toggled_1(object sender, RoutedEventArgs e)
+        {
+            if (NotificationsSwitch != null)
+                SaveSettings(NotificationsSwitch.DataContext as AppSettings);
+        }
+
+        private void Slider_ValueChanged_1(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            if (NotificationsSwitch != null)
+                SaveSettings(NotificationsSwitch.DataContext as AppSettings);
+        }
+
+
     }
 }
