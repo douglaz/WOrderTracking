a9608153862efc4fe556838952d1ca6a4a022e8d
Allan Douglas R. de Oliveira <allandouglas@gmail.com>
2013-07-25 14:51:11 -0300

    - Added AppSettingsDAO

---
+++ WOrderTracking/Persistence/AppSettingsDAO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using WOrderTracking.Model;
+
+namespace WOrderTracking.Persistence
+{
+    public class AppSettingsDAO
+    {
+        private static Windows.Storage.ApplicationDataContainer roamingSettings =
+                Windows.Storage.ApplicationData.Current.RoamingSettings;
+
+        public AppSettings Get()
+        {
+            var settingsValue = roamingSettings.Values["settings"] as ApplicationDataCompositeValue;
+            if (settingsValue != null)
+                return new AppSettings() { EnableNotifications = (bool)settingsValue["enableNotifications"], NotificationsInterval = (int)settingsValue["notificationsInterval"] };
+            else
+                return new AppSettings();
+        }
+
+        public void Save(AppSettings settings)
+        {
+            var settingsValue = new ApplicationDataCompositeValue();
+            settingsValue["enableNotifications"] = settings.EnableNotifications;
+            settingsValue["notificationsInterval"] = settings.NotificationsInterval;
+            roamingSettings.Values["settings"] = settingsValue;
+        }
+    }
+}
--- WOrderTracking/Settings.xaml.cs
+++ WOrderTracking/Settings.xaml.cs
@@ -14,37 +14,22 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using WOrderTracking.Model;
+using WOrderTracking.Persistence;
 
 namespace WOrderTracking
 {
     public sealed partial class Settings : UserControl
     {
-        private static Windows.Storage.ApplicationDataContainer roamingSettings =
-                Windows.Storage.ApplicationData.Current.RoamingSettings;
+        private static AppSettingsDAO settingsDAO = new AppSettingsDAO();
+
         public Settings()
         {
             this.InitializeComponent();
-            var settings = LoadSettings();
+            var settings = settingsDAO.Get();
             NotificationsSwitch.DataContext = settings;
             NotificationsIntervalSlider.DataContext = settings;
         }
 
-        private AppSettings LoadSettings()
-        {
-            var settingsValue = roamingSettings.Values["settings"] as ApplicationDataCompositeValue;
-            if (settingsValue != null)
-                return new AppSettings() { EnableNotifications = (bool)settingsValue["enableNotifications"], NotificationsInterval = (int)settingsValue["notificationsInterval"] };
-            else
-                return new AppSettings();
-        }
-
-        private void SaveSettings(AppSettings settings)
-        {
-            var settingsValue = new ApplicationDataCompositeValue();
-            settingsValue["enableNotifications"] = settings.EnableNotifications;
-            settingsValue["notificationsInterval"] = settings.NotificationsInterval;
-            roamingSettings.Values["settings"] = settingsValue;
-        }
 
         private void MySettingsBackClicked(object sender, RoutedEventArgs e)
         {
@@ -58,13 +43,13 @@ namespace WOrderTracking
         private void ToggleSwitch_Toggled_1(object sender, RoutedEventArgs e)
         {
             if (NotificationsSwitch != null)
-                SaveSettings(NotificationsSwitch.DataContext as AppSettings);
+                settingsDAO.Save(NotificationsSwitch.DataContext as AppSettings);
         }
 
         private void Slider_ValueChanged_1(object sender, RangeBaseValueChangedEventArgs e)
         {
             if (NotificationsSwitch != null)
-                SaveSettings(NotificationsSwitch.DataContext as AppSettings);
+                settingsDAO.Save(NotificationsSwitch.DataContext as AppSettings);
         }
 
 
